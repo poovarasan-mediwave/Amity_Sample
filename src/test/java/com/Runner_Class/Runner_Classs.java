77249fa3c24f28fc65c36419e89e9017f87b9c3c
Amity_Resources <poovarasan@mindwaveventures.com>
2023-01-24 22:21:56 +0530

    runner class change

--- src/test/java/com/Runner_Class/Runner_Classs.java
+++ src/test/java/com/Runner_Class/Runner_Classs.java
@@ -1,27 +1,13 @@
 package com.Runner_Class;
 
-
 import org.junit.runner.RunWith;
 
-import com.Configuration_Reader.FileReaderManager;
-import com.Lib_Global.Base_Class;
-import com.Single_Ton.Single_Ton_Design;
-
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src\\test\\resources\\Feature_File", glue = {
-		"com.Step_Definition" }, plugin = { "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" })
-public class Runner_Classs extends Base_Class{
-	
-	
-	
-
-	
+@CucumberOptions(features = "src\\test\\resources\\Feature_File",monochrome = true, dryRun = false, glue = { "com.Step_Definition" }, plugin = {
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" })
+public class Runner_Classs {
 
 }
-
-
